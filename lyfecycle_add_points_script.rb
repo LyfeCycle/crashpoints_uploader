134f186514827d40e87e2c5d50181cd8305b47eb
Deven <devend711@gmail.com>
2015-02-22 13:37:37 -0500

    skip blank addresses

--- lyfecycle_add_points_script.rb
+++ lyfecycle_add_points_script.rb
@@ -10,12 +10,15 @@ base_uri = 'http://lyfecycle-api.herokuapp.com/locations'
 
 # open spreadsheet
 spreadsheet = Roo::Spreadsheet.open("./bike-collision-database.xlsx")
+end_row = 1805
 
 # init the json for each new location
 point = {name: 'Boston Crash Point'}
 
 # loop through each row
-spreadsheet.sheet(0).each(address: 'Address', doored: 'Doored', area: 'PlanningDi') do |row|
+spreadsheet.sheet(0).each_with_index(address: 'Address', doored: 'Doored', area: 'PlanningDi') do |row, index|
+    break if index == end_row
+    next if !row[:address] # skip if there's no address
     # look up coordinates
     location = Geocoder.search("#{row[:address]} #{row[:area]} MA").first.data['geometry']['location']
     # construct the json for the new location
